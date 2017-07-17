2b2449d6047c73bee37d2cfe5ed0bb38e6120bd3
Tong Shen <tshen@tshen.local>
2017-07-17 17:39:17 +0800

    add email and password to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  String :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711055137) do
+ActiveRecord::Schema.define(version: 20170717092559) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170711055137) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
 end
